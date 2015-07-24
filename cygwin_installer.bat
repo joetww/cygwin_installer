0259c2167e1c35ee712e03adf98b0830458ffc7e
joetww <joetww@gmail.com>
2015-07-24 19:11:59 +0800

    UAC權限檢查
    UAC權限檢查

--- cygwin_installer.bat
+++ cygwin_installer.bat
@@ -4,6 +4,28 @@ REM -- Source: https://github.com/rtwolf/auto-cygwin-install
 
 SETLOCAL
 
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO Administrator PRIVILEGES Detected! 
+) ELSE (
+   echo ######## ########  ########   #######  ########  
+   echo ##       ##     ## ##     ## ##     ## ##     ## 
+   echo ##       ##     ## ##     ## ##     ## ##     ## 
+   echo ######   ########  ########  ##     ## ########  
+   echo ##       ##   ##   ##   ##   ##     ## ##   ##   
+   echo ##       ##    ##  ##    ##  ##     ## ##    ##  
+   echo ######## ##     ## ##     ##  #######  ##     ## 
+   echo.
+   echo.
+   echo ####### ERROR: ADMINISTRATOR PRIVILEGES REQUIRED #########
+   echo This script must be run as administrator to work properly!  
+   echo If you're seeing this after clicking on a start menu icon, then right click on the shortcut and select "Run As Administrator".
+   echo ##########################################################
+   echo.
+   PAUSE
+   EXIT /B 1
+)
+
 REM -- Change to the directory of the executing batch file
 CD %~dp0
 
