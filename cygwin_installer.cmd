d118efbb29b2c9d2e244249b65a2c6e29abf03e5
joetww <joetww@users.noreply.github.com>
2016-01-04 12:53:25 +0800

    Update cygwin_installer.cmd

--- cygwin_installer.cmd
+++ cygwin_installer.cmd
@@ -138,7 +138,7 @@ EXIT /B 0
 	set PATH=%ROOTDIR%\bin;%PATH%
 	"%ROOTDIR%\bin\bash.exe" --login -c "/usr/bin/lynx -source https://raw.githubusercontent.com/transcode-open/apt-cyg/master/apt-cyg > apt-cyg"
 	"%ROOTDIR%\bin\bash.exe" --login -c "/usr/bin/install apt-cyg /bin"
-	"%ROOTDIR%\bin\bash.exe" --login -c "cp /usr/share/vim/vim74/vimrc_example.vim ~/.vimrc"
+	"%ROOTDIR%\bin\bash.exe" --login -c "cp /usr/share/vim/vim7[456789]/vimrc_example.vim ~/.vimrc"
 	"%ROOTDIR%\bin\bash.exe" --login -c "sed -i 's/set mouse=a/set mouse-=a/' ~/.vimrc"
 	"%ROOTDIR%\bin\bash.exe" --login -c "echo > /etc/motd"
 	"%ROOTDIR%\bin\bash.exe" --login -c "git clone https://github.com/joetww/cygwin_installer.git && ( test -d cygwin_installer && cp cygwin_installer/minttyrc ~/.minttyrc && cp cygwin_installer/bash.bashrc /etc/bash.bashrc)"
